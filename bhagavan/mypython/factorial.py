c22aeb715054e67a7097056dd6906fbcde656838
bhagavan <bhagavan@auranetworks.in>
2024-03-14 07:49:52 +0530

    AD-11: Integrating review comments

--- bhagavan/mypython/factorial.py
+++ bhagavan/mypython/factorial.py
@@ -1,11 +1,12 @@
 def factorial(n):
-    print("n :", n)
+    print(f"n :{n}")
     
     f = 1
     for i in range(1, n+1):
         f = f * i
     return f
 
-n = 5
-t = factorial(n)
-print(t)
+if __name__ == "__main__":
+    n = 5
+    t = factorial(n)
+    print(f"fact value of {n} is {t}")
